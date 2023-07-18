9b03d898a976c0ed519298e210da8bc42adbd039
Philip Kuhle <philip.kuhle@geckorobotics.com>
2023-07-18 16:57:24 -0400

    live plotting, read data in fixed bytes

--- python/get_sensor_data.py
+++ python/get_sensor_data.py
@@ -5,46 +5,74 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import math
 
-# Sensor sensitivities [LSB / G]
-sens_500 = 4
-sens_1000 = 2
-sens_2000 = 1
-sensitivities = [sens_500, sens_1000, sens_2000]
+# Parameters
+x_len = 200
+y_range = [0, 400]
 
+
+# Create figure for plotting
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-time_axis = []
-b_field = []
+xs = list(range(0, 200))
+b_500 = [0] * x_len
+b_1000 = [0] * x_len
+b_2000 = [0] * x_len
+ax.set_ylim(y_range)
+
+# Create blank lines to update live
+line_500,  = ax.plot(xs, b_500, label="ALS31313KLEATR-500")
+line_1000, = ax.plot(xs, b_1000, label="ALS31313KLEATR-1000" )
+line_2000, = ax.plot(xs, b_2000, label="ALS31313KLEATR-2000")
+
+# Plot labels
+plt.title("Magnetic Field Strength over Time")
+plt.xlabel("Samples")
+plt.ylabel("B-Field Strength [G]")
+plt.legend()
 
 # get available serial ports
 def get_ports():
     return serial.tools.list_ports.comports()
 
-def animate(i, time_axis, b_field):
-    time_axis.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
-    data_point = get_data_point()
-    b_field.append(data_point)
+def animate(i, b_500, b_1000, b_2000):
+    # get B-field reading from each sensor
+    sensor_data = get_data_point()
+    (sensor_500, sensor_1000, sensor_2000) = sensor_data
 
-    time_axis = time_axis[-20:]
-    b_field = b_field[-20:]
-    ax.clear()
-    ax.plot(time_axis, b_field)
+    b_500.append(sensor_500)
+    b_1000.append(sensor_1000)
+    b_2000.append(sensor_2000)
 
-    plt.xticks(rotation=45, ha='right')
-    plt.subplots_adjust(bottom=0.30)
-    plt.title('Magnetic Field Strength')
-    plt.ylabel('Total Magnetic Field [G]')
+    b_500  = b_500[-x_len:]
+    b_1000 = b_1000[-x_len:]
+    b_2000 = b_2000[-x_len:]
+
+    line_500.set_ydata(b_500)
+    line_1000.set_ydata(b_1000)
+    line_2000.set_ydata(b_2000)
+
+    return (line_500, line_1000, line_2000)
 
 def get_data_point():
-    data = s.readline()
-    print(f"Incoming data: {data}")
-    data = "".join([hex(x)[2:] for x in list(data)[:-1]])
-    if data == 'b\'\\n\'':
-        return 0
-    Bt_squared = int(data, 16)
-    Bt = math.sqrt(Bt_squared)
-    print(f"Converted data: {Bt}")
-    return Bt
+    data = s.read(12) # read stream of 12 bytes
+    print(f"Raw data: {data}")
+    data_list = list(data)
+
+    # extract data components
+    data_500 = data_list[0:4]
+    data_1000 = data_list[4:8]
+    data_2000 = data_list[8:13]
+
+    # convert data to ints
+    data_500 = "".join(hex(x)[2:] for x in data_500)
+    data_500 = round(math.sqrt(int(data_500, 16)), 2)
+    data_1000 = "".join(hex(x)[2:] for x in data_1000)
+    data_1000 = round(math.sqrt(int(data_1000, 16)), 2)
+    data_2000 = "".join(hex(x)[2:] for x in data_2000)
+    data_2000 = round(math.sqrt(int(data_2000, 16)), 2)
+    b_fields = (data_500, data_1000, data_2000)
+
+    return b_fields # in the form (ALS31313KLEATR-500 reading, ALS31313KLEATR-1000 reading, ALS31313KLEATR-2000)
     
 
 ports = get_ports()
@@ -55,5 +83,5 @@ if (not(s.is_open)):
     s.open()
 
 while True:
-    anim = animation.FuncAnimation(fig, animate, fargs=(time_axis, b_field), interval=750)
+    anim = animation.FuncAnimation(fig, animate, fargs=(b_500, b_1000, b_2000), interval=50, blit=True)
     plt.show()
