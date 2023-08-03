5cac80368847ff2bd364fac9add9857c06da1e28
Philip Kuhle <philip.kuhle@geckorobotics.com>
2023-08-03 14:13:33 -0400

    testing for peak detection

--- python/peak_detection_test.py
+++ python/peak_detection_test.py
@@ -1,16 +1,26 @@
 from peak_detection_algorithm import thresholding_algo
 import numpy as np
 import pylab
+import os
+import pandas as pd
 
+dir = "/home/pkuhle/src/wheel-adhesion/wheel_adhesion_data/WAD_PCB_data/raw/differential/large_weld_line"
+files = os.listdir(dir)
+data = pd.read_csv(os.path.join(dir, files[0]))
 
-y = np.array([1218.29, 1219.49, 1215.46, 1218.59, 1221.59, 1228.22, 1230.85, 1230.74, 1226.36, 1240.37, 1267.84, 1263.84, 1259.17, 1255.95, 1252.94, 1253.83, 1256.55, 1253.39, 1258.68, 1258.14, 1266.45, 1259.24, 1261.87, 1257.11, 1251.25, 1248.3, 1252.36, 1216.69, 1218.53, 1220.81, 1223.87, 1221.87, 1219.02, 1222.47, 1223.8, 1221.03, 1220.68, 1214.76, 1215.24, 1217.75, 1213.96, 1210.61, 1217.42])
+y = data.loc[:, "Bt-top"]
+y = y.to_numpy()
 
-lag = 3
-threshold = 7
-influence = 0.7
+# adjustable parameters
+lag = 5
+threshold = 2.9
+influence = 0.6
 
 # Run algo with settings from above
 result = thresholding_algo(y, lag=lag, threshold=threshold, influence=influence)
+lower_bound = result["avgFilter"] - (threshold * result["stdFilter"])
+upper_bound = result["avgFilter"] + (threshold * result["stdFilter"])
+
 
 # Plot result
 pylab.subplot(211)
@@ -20,15 +30,17 @@ pylab.plot(np.arange(1, len(y)+1),
            result["avgFilter"], color="cyan", lw=2)
 
 pylab.plot(np.arange(1, len(y)+1),
-           result["avgFilter"] + threshold * result["stdFilter"], color="green", lw=2)
+           upper_bound, color="green", lw=2)
 
 pylab.plot(np.arange(1, len(y)+1),
-           result["avgFilter"] - threshold * result["stdFilter"], color="orange", lw=2)
+           lower_bound, color="orange", lw=2)
 
 pylab.subplot(212)
 pylab.step(np.arange(1, len(y)+1), result["signals"], color="red", lw=2)
 pylab.ylim(-1.5, 1.5)
 pylab.show()
+
+
 
 
 
